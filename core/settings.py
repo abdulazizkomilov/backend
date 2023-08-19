16f90cb431b9fb186dde202de04bf0af488c6943
Abdulaziz Komilov <80712573+abdulazizkomilov@users.noreply.github.com>
2023-08-19 13:45:34 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from pathlib import Path
 from os import getenv, path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
